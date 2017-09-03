e6ca418fcb62b5463a667fbb42490d68d789f143
Edward Burton <erburton@gmail.com>
2017-09-03 14:15:44 -0500

    First Commit

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,18 @@
 var animatePoints = function() {
-
     var points = document.getElementsByClassName('point');
-
     var revealPoint = function( i ) {
         points[i].style.opacity = 1;
         points[i].style.transform = "scaleX(1) translateY(0)";
         points[i].style.msTransform = "scaleX(1) translateY(0)";
         points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
     };
-
     for (var i = 0; i < points.length; i++) {
       revealPoint( i );
     }
-
 };
+
+window.onload = function() {
+  window.addEventListener('scroll', function(event) {
+      console.log(event);
+  });
+ }
