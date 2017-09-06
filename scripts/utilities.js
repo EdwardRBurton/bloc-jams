98efc070d1b2ff152b0ff513be81a450ceb6fdf0
Edward Burton <erburton@gmail.com>
2017-09-06 10:34:29 -0500

    Video solution code

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,13 +1,5 @@
-var forEacha = function forEacha() {
-  console.log("This section of code is working");
-  //loop through all elements in points array, NOT poiintsArray!!
-  for (var i = 0; i < points.length; i++) {
-    //execute a callback for each elements
-    function myForEachCallback(points, callback) {
-//what does this function need to do? Not console.log the points array
-      console.log(points);
-      callback();
-    }
+function forEach(array, callback) {
+  for (var i = 0; i < array.length; i++) {
+    callback(array[i]);
   }
-  //replace the for loop in landing.js with foreach block created here
 }
