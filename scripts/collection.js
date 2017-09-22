f7f793edfa6565a1371e652c202f54d33d5abaa3
Edward Burton <erburton@gmail.com>
2017-09-21 22:09:33 -0500

    Checkpoint 17 almost done

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,26 +1,29 @@
-var collectionItemTemplate =
-    '<div class="collection-album-container column fourth">'
-  + '  <img src="assets/images/album_covers/01.png"/>'
-  + '  <div class="collection-album-info caption">'
-  + '    <p>'
-  + '      <a class="album-name" href="album.html"> The Colors </a>'
-  + '      <br/>'
-  + '      <a href="album.html"> Pablo Picasso </a>'
-  + '      <br/>'
-  + '      X songs'
-  + '      <br/>'
-  + '    </p>'
-  + '  </div>'
-  + '</div>'
-  ;
-
-  window.onload = function() {
-    // selects the first album-covers element and assigns it to collectionContainer
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    // clear the content from innerHTML
-    collectionContainer.innerHTML = '';
-    // insets collectionItemTemplate into the innerHTML of collectionContainer
-    for (var i = 0; i <12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
-    }
+var buildCollectionItemTemplate = function() {
+  var template =
+    '<div class="collection-album-container column fourth">' +
+    '  <img src="assets/images/album_covers/01.png"/>' +
+    '  <div class="collection-album-info caption">' +
+    '    <p>' +
+    '      <a class="album-name" href="album.html"> The Colors </a>' +
+    '      <br/>' +
+    '      <a href="album.html"> Pablo Picasso </a>' +
+    '      <br/>' +
+    '      X songs' +
+    '      <br/>' +
+    '    </p>' +
+    '  </div>' +
+    '</div>';
+  return $(template);
+};
+$(window).load(function() {
+  // selects the first album-covers element and assigns it to collectionContainer
+  var $collectionContainer = $('.album-covers');
+  // clear the content from innerHTML
+  $collectionContainer.empty();
+  // insets collectionItemTemplate into the innerHTML of collectionContainer
+  for (var i = 0; i < 12; i++) {
+    var $newThumbnail = buildCollectionItemTemplate();
+    $collectionContainer.append($newThumbnail);
+  }
 }
+});
