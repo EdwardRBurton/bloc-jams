d7766000d7a1ab31d4e59948c248095e570fc796
Edward Burton <erburton@gmail.com>
2017-09-24 15:22:55 -0500

    Checkpoint 18 stopped at write on:offhover

--- scripts/album.js
+++ scripts/album.js
@@ -39,8 +39,30 @@
      + '</tr>'
      ;
 
-    return $(template);
-};
+    var $row = $(template);
+
+    var onHover = function(event) {
+      var songNumberCell = $(this).fin('.song-item-number');
+      var songNumber = songNumberCell.after('data-song-number');
+
+      if (songNumber !== currentlyPlayingSong) {
+        songNumberCell.html(playButtonTemplate);
+      }
+    };
+
+    var offHover = function(event) {
+      var songNumberCell = $(this).find('.song-item-number');
+      var songNumber = songNumberCell.attr('data-song-number');
+
+      if (songNumber !== currentlyPlayingSong) {
+        songNumberCell.html(songNumber);
+      }
+    };
+
+    $row.find('.song-item-number').click(clickHandler);
+    $row.hover(onHover, offHover);
+    return $row;
+    };
 
 var setCurrentAlbum = function(album) {
     //  select all of the HTML elements required to display on the album page
